793628e21f0544b442135ca9cd394ae335dd8e49
esteban1011 <esteban.m.g4@gmail.com>
2021-06-06 00:05:16 -0400

    trabajo

---
+++ promedio2.rb
@@ -0,0 +1,47 @@
+# b = [100, 800, 250, 300, 500, 250]
+# a = [200, 400, 40, 300, 500, 600]
+
+
+# def promedio(array)
+#     con = 0
+#     x = array.count
+#         x.times do |i|
+#         con = con + array[i].to_i
+#     end
+#     promedioa = con / x
+
+#     con = 0
+#     x = array.count
+#         x.times do |i|
+#         con = con + array[i].to_i
+#     end
+#     promediob = con / x
+
+
+# end
+
+# puts promedio ()
+
+
+a = [100, 800, 250, 300, 500, 2500]
+b = [200, 400, 40, 300, 500, 60]
+
+
+def promedio(array)
+    con=0
+    x=array.count
+        x.times do |i|
+        con = con + array[i].to_i
+        end
+    promedio = con/x
+end
+
+def compara_arrays(a,b)
+    if promedio(a)>promedio(b)
+        puts promedio(a)
+    else
+        puts promedio(b)
+    end
+end
+
+compara_arrays(a,b)
---
+++ visitas.rb
@@ -0,0 +1,24 @@
+visitas = [1000, 800, 250, 300, 500, 2500]
+def promedio(array)
+    con=0
+    x=array.count
+    x.times do |i|
+    con = con + array[i].to_i
+    end
+    promedio = con/x
+end
+
+puts promedio(visitas)
+
+# visitas = [1000, 800, 250, 300, 500, 2500]
+
+# def promedio(array)
+#     con=0
+#     x=array.count
+#         x.times do |i|
+#         con = con + array[i].to_i
+#         end
+#     promedio = con/x
+# end
+
+# puts promedio(visitas)
